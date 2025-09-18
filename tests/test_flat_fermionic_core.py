c990c135f5f60634a0255ba3beab7dfb987e74e2
Johnnie Gray <johnniemcgray@gmail.com>
2025-09-18 15:03:59 -0700

    test flat fermionic phase_sync

--- tests/test_flat_fermionic_core.py
+++ tests/test_flat_fermionic_core.py
@@ -6,7 +6,13 @@ from .test_flat_abelian_core import get_zn_blocksparse_flat_compat
 @pytest.mark.parametrize("symmetry", ["Z2", "Z3", "Z4"])
 @pytest.mark.parametrize("charge", [0, 1])
 @pytest.mark.parametrize("seed", [42, 43, 44])
-def test_to_and_from_blocksparse(symmetry, charge, seed):
+@pytest.mark.parametrize("sync", [False, True])
+def test_to_and_from_blocksparse_with_phase_sync(
+    symmetry,
+    charge,
+    seed,
+    sync,
+):
     if charge:
         pytest.xfail("oddpos not implemented yet.")
 
@@ -21,7 +27,13 @@ def test_to_and_from_blocksparse(symmetry, charge, seed):
     assert x.phases
     fx = x.to_flat()
     assert fx.fermionic
+    if sync:
+        fx.phase_sync(inplace=True)
     fx.check()
     y = fx.to_blocksparse()
+    if sync:
+        assert not y.phases
+    else:
+        assert y.phases
     y.check()
     assert y.allclose(x)
