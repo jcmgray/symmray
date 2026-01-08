eb75bbba2c9cee865cebba4cc992d61694701ffe
Johnnie Gray <johnniemcgray@gmail.com>
2026-01-08 14:29:42 -0800

    add random regular graph fermionic amps test

--- tests/test_quimb_integration/test_quimb_fermionic_amplitudes.py
+++ tests/test_quimb_integration/test_quimb_fermionic_amplitudes.py
@@ -22,7 +22,7 @@ def test_2d_spinless(cyclic, flat, fermionic, site_charge):
         def site_charge_fn(site):
             return site_charge
 
-    peps = sr.networks.PEPS_abelian_rand(
+    psi = sr.networks.PEPS_abelian_rand(
         "Z2",
         Lx,
         Ly,
@@ -35,24 +35,100 @@ def test_2d_spinless(cyclic, flat, fermionic, site_charge):
         fermionic=fermionic,
     )
 
-    charge = sum(x.charge for x in peps.arrays) % 2
+    charge = sum(x.charge for x in psi.arrays) % 2
 
     # contract full wavefunction
-    t = peps.contract(all, output_inds=peps.site_inds)
+    t = psi.contract(all, output_inds=psi.site_inds)
     x = t.data
     if fermionic:
         x.phase_sync(inplace=True)
 
     # for every bit-string, check sliced-then-contract == contract-then-slice
-    N = peps.nsites
+    N = psi.nsites
     for r in range(2 ** (N - 1)):
         # for Z2, last bit fixed by charge conservation
         prefix = list(map(int, np.binary_repr(r, width=N - 1)))
         config = (*prefix, (charge + int(np.sum(prefix) % 2)) % 2)
 
         # slice wavefunction and contract
-        psix = peps.isel(
-            {peps.site_ind(coo): v for coo, v in zip(peps.sites, config)}
+        psix = psi.isel(
+            {psi.site_ind(coo): v for coo, v in zip(psi.sites, config)}
+        )
+        amp = psix.contract(all, output_inds=())
+
+        # retrieve relevant amplitude from full contraction
+        if flat:
+            x.sort_stack(inplace=True)
+            famp = x.blocks[r].item()
+        else:
+            famp = x.blocks[config].item()
+
+        # compare
+        assert abs(1 - famp / amp) < 1e-10
+
+
+edges_rrg_d3_n10_seed42 = [
+    (0, 7),
+    (0, 3),
+    (0, 6),
+    (1, 5),
+    (1, 3),
+    (1, 9),
+    (2, 6),
+    (2, 5),
+    (2, 8),
+    (3, 4),
+    (4, 9),
+    (4, 8),
+    (5, 9),
+    (6, 7),
+    (7, 8),
+]
+
+
+@pytest.mark.parametrize("flat", [False, True])
+@pytest.mark.parametrize("fermionic", [False, True])
+@pytest.mark.parametrize("site_charge", [0, 1, "checkerboard"])
+def test_rrg_spinless(flat, fermionic, site_charge):
+    if site_charge == "checkerboard":
+
+        def site_charge_fn(site):
+            return site % 2
+
+    else:
+
+        def site_charge_fn(site):
+            return site_charge
+
+    psi = sr.networks.TN_abelian_from_edges_rand(
+        "Z2",
+        edges_rrg_d3_n10_seed42,
+        bond_dim=2,
+        phys_dim=2,
+        seed=42,
+        site_charge=site_charge_fn,
+        flat=flat,
+        fermionic=fermionic,
+    )
+
+    charge = sum(x.charge for x in psi.arrays) % 2
+
+    # contract full wavefunction
+    t = psi.contract(all, output_inds=psi.site_inds)
+    x = t.data
+    if fermionic:
+        x.phase_sync(inplace=True)
+
+    # for every bit-string, check sliced-then-contract == contract-then-slice
+    N = psi.nsites
+    for r in range(2 ** (N - 1)):
+        # for Z2, last bit fixed by charge conservation
+        prefix = list(map(int, np.binary_repr(r, width=N - 1)))
+        config = (*prefix, (charge + int(np.sum(prefix) % 2)) % 2)
+
+        # slice wavefunction and contract
+        psix = psi.isel(
+            {psi.site_ind(coo): v for coo, v in zip(psi.sites, config)}
         )
         amp = psix.contract(all, output_inds=())
 
